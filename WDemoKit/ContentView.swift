5cb70c00664928fee88b98bc7770cbf84d223b08
Heshuwang <heshuwang123@163.com>
2019-11-29 10:58:26 +0800

    测试

---
+++ WDemoKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WDemoKit
+//
+//  Created by hs on 2019/11/29.
+//  Copyright © 2019 hs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
